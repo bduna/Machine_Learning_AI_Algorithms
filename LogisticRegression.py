801d2d55d18c8b30e021191ba2f5e9fd5bf23acf
Barrett Duna <barrett.duna@gmail.com>
2020-05-03 10:51:05 -0700

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -7,21 +7,22 @@ class LogisticRegression:
         self.eta = eta
         self.num_epochs = num_epochs
         self.weights = None
-        self.cost_ = None
+        self._cost = None
 
     def fit(self, X, y):
         y = np.array(y)
-        self.cost_ = []
-        X.insert(0, "bias", 1)
+        self._cost = []
+        X = np.array(X)
+        X = np.insert(X, 0, 1, 1)
         self.weights = np.random.normal(loc=0, scale=0.01, size=X.shape[1])
         for _ in range(self.num_epochs):
             net_input = self.net_input(X)
             output = self.activation(net_input)
             errors = y - output
-            delta_weights = self.eta * X.T.dot(errors)
+            delta_weights = self.eta * X.transpose().dot(errors)
             self.weights += delta_weights
             epsilon = 1e-5
-            self.cost_.append(-y.dot(np.log(output + epsilon)) - (1 - y).dot(np.log(1 - output + epsilon)))
+            self._cost.append(-y.dot(np.log(output + epsilon)) - (1 - y).dot(np.log(1 - output + epsilon)))
 
     def net_input(self, X):
         return np.dot(X, self.weights)
@@ -33,48 +34,41 @@ class LogisticRegression:
         if type(new_data) is list:
             new_data.insert(0, 1)
             return np.where(self.activation(self.net_input(new_data)) >= 0.5, 1, 0)
-        elif type(new_data) is pd.DataFrame:
-            predictions = []
-            for index, x in new_data.iterrows():
-                x = list(x)
-                x.insert(0, 1)
-                prediction = np.where(self.activation(self.net_input(x)) >= 0.5, 1, 0)
-                predictions.append(prediction)
-            return predictions
+        elif (type(new_data) is pd.DataFrame) or (type(new_data) is np.ndarray):
+            new_data = np.array(new_data)
+            new_data = np.insert(new_data, 0, 1, 1)
+            predictions = np.where(self.activation(self.net_input(new_data)) >= 0.5, 1, 0)
+            return list(predictions)
         else:
-            print("Input must be one dimensional list or Pandas DataFrame.")
+            print("Input must be one dimensional list, Numpy Array or Pandas DataFrame.")
 
     def accuracy(self, X_test, y_test):
-        num_trials = len(y_test)
-        correct = 0
-        if (type(X_test) is pd.DataFrame) and (type(y_test) == pd.core.series.Series):
-            for index, x in X_test.iterrows():
-                x = list(x)
-                if self.predict(x) == y_test[index]:
-                    correct += 1
-            return correct / num_trials
+        if (type(X_test) is pd.DataFrame) or (X_test is np.ndarray):
+            X_test = np.array(X_test)
+            return sum(logit_model.predict(X_test) == y_test)/len(y_test)
         else:
-            print("Input must be Pandas DataFrame and Series.")
+            print("Feature input must be Pandas DataFrame or Numpy ndarray.")
 
 if __name__ == "__main__":
 
-    data = pd.read_csv("iris.csv")
-
     from sklearn.model_selection import train_test_split
+    
+    iris = pd.read_csv("iris.csv")
 
     logit_model = LogisticRegression(num_epochs=100, eta=0.01)
-    X = data[["sepal_length", "sepal_width"]]
+    X = iris[["sepal_length", "sepal_width"]]
     X = X.iloc[:100]
-    y = data["class"].iloc[:100].apply(lambda c: 1 if c == "Iris-setosa" else 0)
+    y = iris["class"].iloc[:100].apply(lambda c: 1 if c == "Iris-setosa" else 0)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
-
+    
     logit_model.fit(X_train, y_train)
-
-    print("Logistic regression weights:\n", logit_model.weights)
-    print("Accuracy on test data: ", logit_model.accuracy(X_test, y_test))
-
-
+    
+    print(logit_model.weights)
+    
+    print("Logistic Regression model accuracy: {:.0%}".format(logit_model.accuracy(X_test, y_test)))
+    
+    
 
 
 
